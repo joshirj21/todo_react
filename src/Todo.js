56cd8fef0da68a6863224c7454a9416bdcf001f8
Rahul joshi <joshi.rj21@gmail.com>
2019-11-21 13:25:56 +0530

    Added todo li's

--- src/Todo.js
+++ src/Todo.js
@@ -4,9 +4,23 @@ class Todo extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleRemoveClick = this.handleRemoveClick.bind(this);
+        this.handleDoneClick = this.handleDoneClick.bind(this)
+    }
+    handleRemoveClick() {
+        this.props.remove(this.props.id)
+    }
+    handleDoneClick(ev) {
+        ev.target.style.textDecoration = "line-through";
+        ev.target.style.fontStyle = "oblique"
     }
     render() {
-        return <h1></h1>
+        return (<div>
+            <li onClick={this.handleDoneClick}>
+                {this.props.text}
+            </li>
+            <button onClick={this.handleRemoveClick}>X</button>
+        </div>)
     }
 }
 
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import Todo from "./Todo"
 import NewTodoForm from "./NewTodoForm"
+import uuid from "uuid/v4";
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = { todos: [] };
         this.handleForm = this.handleForm.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
     }
     handleForm(todo) {
-        this.setState((st) => { return { todos: [...st.todos, todo] } })
+        this.setState((st) => {
+            let obj = { ...todo, id: uuid() }
+            return { todos: [...st.todos, obj] }
+        })
+    }
+    handleRemove(Id) {
+        let arr = this.state.todos.filter((el) => el.id !== Id)
+        this.setState({ todos: arr })
     }
     render() {
         return (
             <div>
                 <NewTodoForm handleForm={this.handleForm} />
-                <Todo />
+                <ul>
+                    {this.state.todos.map((el) => <Todo text={el.todo} key={el.id} id={el.id} remove={this.handleRemove} />)}
+                </ul>
             </div>
         )
     }
